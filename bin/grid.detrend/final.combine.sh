2bb1e114e9dcd5fe0625829e3da871bcab00bd02
James Chryssanthacopoulos <jchryssanthacopoulos@gmail.com>
2014-11-07 17:16:43 -0600

    Small update to final combine step in grid cell detrending to delete temp files

--- bin/grid.detrend/final.combine.sh
+++ bin/grid.detrend/final.combine.sh
@@ -11,6 +11,9 @@ for f in `ls /project/joshuaelliott/ggcmi/reference/iizumi/30min/*.nc4`; do
    ncpdq -O -h -a lat,lon $iizumi/$fn $iizumi/$fn
 done
 
+rm $iizumi/*.nc4.*
+for d in {1..8}; do rm -r $iizumi/$d; done
+
 for f in `ls /project/joshuaelliott/ggcmi/reference/ray/weighted/*.nc4`; do
    fn=$(basename $f)
    echo $fn
@@ -18,3 +21,6 @@ for f in `ls /project/joshuaelliott/ggcmi/reference/ray/weighted/*.nc4`; do
    ncpdq -O -h -a time,lat $ray/$fn $ray/$fn
    ncpdq -O -h -a lat,lon $ray/$fn $ray/$fn
 done
+
+rm $ray/*.nc4.*
+for d in {1..8}; do rm -r $ray/$d; done
