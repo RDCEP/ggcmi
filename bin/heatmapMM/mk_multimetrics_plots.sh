2d0afda2949a72fe7e34914d0746e3233ea74428
James Chryssanthacopoulos <jchryssanthacopoulos@gmail.com>
2014-11-20 09:13:36 -0600

    Fixed some bugs in run scripts

--- bin/heatmapMM/mk_multimetrics_plots.sh
+++ bin/heatmapMM/mk_multimetrics_plots.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for g in 240 48; do # US, china
-  ./multimetrics_plot.py -d /project/ggcmi/AgMIP.output/processed/multimetrics/gadm0/faostat -v tscorr -x model -y climate,agcfsr,agmerra,cfsr,erai,grasp,princeton,watch,wfdei.cru,wfdei.gpcc -c gadm0,$g -o scen -o dt -o mp -c cr,mean-scale -c time_range,full -c crop,mai -o . --anon -f eps
+  ./multimetrics_plot.py -d /project/ggcmi/AgMIP.output/processed/multimetrics/gadm0/faostat -v tscorr -x model -y climate,agcfsr,agmerra,cfsr,erai,grasp,princeton,watch,wfdei.cru,wfdei.gpcc -c gadm0,$g -o scen -o dt -o mp -c cr,mean-scale -c time_range,full -c crop,mai --outdir . -f eps --anon
 done
--- bin/rescaler/rescaler.sh
+++ bin/rescaler/rescaler.sh
@@ -3,7 +3,7 @@
 PATH=$PATH:/project/joshuaelliott/ggcmi/utils
 
 swift -tc.file tc.data -sites.file midway.xml -config swift.properties rescaler.swift \
-      -mkfile=/project/joshuaelliott/ggcmi/processed/masks/aggr/gadm0.mask.nc4        \
+      -mkfile=/project/ggcmi/AgMIP.output/processed/masks/aggr/gadm0.mask.nc4         \
       -crmthd=2,0,2                                                                   \
       -agglvl=gadm0
 
