a1b32709c1ff98f48c2ed08a416ff75e4870127f
James Chryssanthacopoulos <jchryssanthacopoulos@gmail.com>
2015-01-15 13:26:43 -0600

    Updated detrending code to detrend reference data aggregated using dynamic mask

--- bin/detrend.reference/detrendref.sh
+++ bin/detrend.reference/detrendref.sh
@@ -3,6 +3,8 @@
 indir=/project/joshuaelliott/ggcmi/bin/agg.reference
 outdir=/project/ggcmi/AgMIP.input/other.inputs/reference
 
+PATH=$PATH:/project/joshuaelliott/ggcmi/utils
+
 for r in ray iizumi; do
    if [ $r = ray ]; then
       yrs=1961-2008
@@ -10,6 +12,15 @@ for r in ray iizumi; do
       yrs=1982-2006
    fi
    for a in fpu kg global; do
-      ./detrendref.py -i $indir/${r}.${yrs}.${a}.nc4 -a $a -c mai,ric,soy,whe -o $outdir/$r/${r}.${yrs}.${a}.2.nc4
+      for w in fixed dynamic; do
+         if [ $w = fixed ]; then
+            ifile=$indir/${r}.${yrs}.${a}.nc4
+            ofile=$outdir/$r/${r}.${yrs}.${a}.nc4
+         else
+            ifile=$indir/${r}.${yrs}.${a}.dynamic.nc4
+            ofile=$outdir/$r/${r}.${yrs}.${a}.dynamic.nc4
+         fi
+         ./detrendref.py -i $ifile -a $a -c mai,ric,soy,whe -o $ofile
+      done
    done
 done
