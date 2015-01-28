5a0cbab4373ac9bea7541ebbdd2e11663f62c1a0
James Chryssanthacopoulos <jchryssanthacopoulos@gmail.com>
2015-01-28 09:42:14 -0600

    Updated input script for biascorrecter

--- bin/biascorr/inputs.sh
+++ bin/biascorr/inputs.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-root=/project/ggcmi
+indir=/project/ggcmi/AgMIP.output/processed/aggs
+outdir=/project/ggcmi/AgMIP.output/processed/biascorr
+refdir=/project/ggcmi/AgMIP.input/other.inputs/reference
+
+refs=(faostat ray iizumi)
+refl=(faostat.1961-2012 ray.1961-2008 iizumi.1982-2006)
 
 # Header
 echo indir reffile agglvl outdir
 
-# gadm0
-echo $root/AgMIP.output/processed/aggs/gadm0 $root/AgMIP.input/other.inputs/reference/faostat/faostat.1961-2012.gadm0.nc4 gadm0 $root/AgMIP.output/processed/biascorr/gadm0/faostat
-
-# fpu
-echo $root/AgMIP.output/processed/aggs/fpu $root/AgMIP.input/other.inputs/reference/ray/ray.1961-2008.fpu.nc4 fpu $root/AgMIP.output/processed/biascorr/fpu/ray
-echo $root/AgMIP.output/processed/aggs/fpu $root/AgMIP.input/other.inputs/reference/iizumi/iizumi.1982-2006.fpu.nc4 fpu $root/AgMIP.output/processed/biascorr/fpu/iizumi
-
-# kg
-echo $root/AgMIP.output/processed/aggs/kg $root/AgMIP.input/other.inputs/reference/ray/ray.1961-2008.kg.nc4 kg $root/AgMIP.output/processed/biascorr/kg/ray
-echo $root/AgMIP.output/processed/aggs/kg $root/AgMIP.input/other.inputs/reference/iizumi/iizumi.1982-2006.kg.nc4 kg $root/AgMIP.output/processed/biascorr/kg/iizumi
-
-# global
-echo $root/AgMIP.output/processed/aggs/global $root/AgMIP.input/other.inputs/reference/faostat/faostat.1961-2012.global.nc4 global $root/AgMIP.output/processed/biascorr/global/faostat
+for a in gadm0 fpu kg global; do
+   for ((ref = 0; ref < ${#refs[@]}; ref++)); do
+      rs=${refs[$ref]}
+      rl=${refl[$ref]}
+      for area in fixed ray iizumi; do
+         reffile=$refdir/$rs/$rl.$a.$area.nc4
+         if [ ! -f $reffile ] || [ ! -d $indir/$a/$area ]; then
+            continue
+         fi
+         mkdir -p $outdir/$a/$rs/$area
+         echo $indir/$a/$area $reffile $a $outdir/$a/$rs/$area
+      done
+   done
+done
