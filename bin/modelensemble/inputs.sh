33dfc9836d00e1fa80e2dce2d80ac13852820d1b
David Kelly <davidkelly999@gmail.com>
2017-03-27 15:07:45 -0500

    Updates to modelensemble to use rmse only and limit dt, mp, and cr

--- bin/modelensemble/inputs.sh
+++ bin/modelensemble/inputs.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-bcdir=/project/ggcmi/AgMIP.output/processed/biascorr
-mmdir=/project/ggcmi/AgMIP.output/processed/multimetrics
-outdir=/project/ggcmi/AgMIP.output/processed/modelensemble
+bcdir=/project/ggcmi/phase1.final/processed.simple/biascorr
+mmdir=/project/ggcmi/phase1.final/processed.simple/multimetrics
+outdir=/project/ggcmi/phase1.final/processed.simple/modelensemble
+aggmasks="gadm0 global"
+refs="faostat"
+areas="fixed_mirca_mask"
 
 # Header
 echo indir metricsdir agglvl outdir
 
-for a in gadm0 fpu kg global; do
-   for ref in faostat ray iizumi; do   
-      for area in fixed_mirca_mask dynamic_ray_mask fixed_iizumi_mask fixed_spam_mask; do
-         if [ ! -d $bcdir/$a/$ref/$area ] || [ ! -d $mmdir/$a/$ref/$area ]; then
+for aggmask in $aggmasks; do
+   for ref in $refs; do
+      for area in $areas; do
+         if [ ! -d $bcdir/$aggmask/$ref/$area ] || [ ! -d $mmdir/$aggmask/$ref/$area ]; then
             continue
          fi
-         mkdir -p $outdir/$a/$ref/$area
-         echo $bcdir/$a/$ref/$area $mmdir/$a/$ref/$area $a $outdir/$a/$ref/$area
+         mkdir -p $outdir/$aggmask/$ref/$area
+         echo $bcdir/$aggmask/$ref/$area $mmdir/$aggmask/$ref/$area $aggmask $outdir/$aggmask/$ref/$area
       done
    done
 done
--- bin/modelensemble/modelensemble.sh
+++ bin/modelensemble/modelensemble.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-PATH=$PATH:/project/joshuaelliott/ggcmi/utils
+PATH=$PATH:/project/joshuaelliott/ggcmi/utils:$PWD
 
-swift -tc.file tc.data -sites.file midway.xml -config swift.properties modelensemble.swift \
+site=$1
+if [ -z "$site" ]; then
+    site="sandyb"
+fi
+
+swift -tc.file tc.data -sites.file ${site}.xml -config swift.properties modelensemble.swift \
       -w=agmerra,agcfsr,cfsr,erai,grasp,watch,wfdei.cru,wfdei.gpcc,princeton \
       -c=mai,whe,ric,soy,sor,mil \
-      -m=rmse,tscorr
+      -m=rmse
 
-if [ $? -eq 0 ]; then
+ec=$?
+echo Cleaning up, please wait...
+sleep 5
+
+if [ "$ec" -eq 0 ]; then
    rm -rf run???
    rm finder.out
 fi
+
+echo Done
